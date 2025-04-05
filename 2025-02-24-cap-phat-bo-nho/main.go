0c7dbc4020145da21ae9068408b12528fe29fdb2
dangars151 <dangars151@gmail.com>
2025-04-05 08:02:07 +0700

    cap phat bo nho

---
+++ 2025-02-24-cap-phat-bo-nho/main.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	//io.Reader()
+
+	//r := strings.NewReader("Hello, Golang!")
+	//
+	//buf := make([]byte, 5) // Buffer 5 byte
+	//for {
+	//	n, err := r.Read(buf)
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//	fmt.Print(string(buf[:n])) // In từng phần của chuỗi
+	//}
+	//
+	//io.Copy(nil, r)
+
+	//x := getValue()
+	//z := *x + 1
+	//println(*x + z)
+}
+
+//func getValue() *int {
+//	y := 1
+//	return &y
+//}
